4bc74fb2013a98c980343653654da8a75b162bc9
John Knotts <jhnnyk@gmail.com>
2024-09-10 16:40:26 -0600

    initial take on start screen

--- lib/main.dart
+++ lib/main.dart
@@ -7,12 +7,29 @@ void main() {
 class MainApp extends StatelessWidget {
   const MainApp({super.key});
 
+  void startQuiz() {}
+
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       home: Scaffold(
+        backgroundColor: const Color.fromARGB(255, 151, 253, 221),
         body: Center(
-          child: Text('Hello World!'),
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Image.asset('assets/images/sk8.png'),
+              const Text(
+                'Learn about skateboarding the dumb way!',
+                style: TextStyle(fontSize: 16),
+              ),
+              const SizedBox(height: 14,),
+              ElevatedButton(
+                onPressed: startQuiz,
+                child: const Text('Start Quiz'),
+              )
+            ],
+          ),
         ),
       ),
     );
