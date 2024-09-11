6e2af66037f80f325c7827a833f6d3aa8b9500d4
John Knotts <jhnnyk@gmail.com>
2024-09-11 13:29:34 -0600

    add data (questions)

---
+++ lib/data/questions.dart
@@ -0,0 +1,40 @@
+import 'package:sk8_quiz_app/models/quiz_question.dart';
+
+const questions = [
+  QuizQuestion(
+    'Who is considered the Godfather of modern street skating?',
+    [
+      'Rodney Mullen',
+      'Tony Hawk',
+      'Mark Gonzales',
+      'Nyjah Huston',
+    ],
+  ),
+  QuizQuestion(
+    'Who is on the cover of the October 2024 issue of Thrasher Magazine?',
+    [
+      'Elijah Berle',
+      'Alexis Sablone',
+      'Louie Lopez',
+      'Miles Silvas',
+    ],
+  ),
+  QuizQuestion(
+    'Who won Thrasher\'s Skater of the Year Award in 2006?',
+    [
+      'Daewon Song',
+      'Chris Cole',
+      'Marc Johnson',
+      'Danny Way',
+    ],
+  ),
+  QuizQuestion(
+    'What year was the first Thrasher Magazine published?',
+    [
+      '1981',
+      '1980',
+      '1979',
+      '1982',
+    ],
+  ),
+];
