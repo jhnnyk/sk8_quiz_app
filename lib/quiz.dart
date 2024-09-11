0b52254a5dbebc8a8d503e55f8d59fd48c6674d1
John Knotts <jhnnyk@gmail.com>
2024-09-11 06:02:22 -0600

    render content conditionally

--- lib/quiz.dart
+++ lib/quiz.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:sk8_quiz_app/questions_screen.dart';
 import 'package:sk8_quiz_app/start_screen.dart';
 
 class Quiz extends StatefulWidget {
@@ -11,12 +12,20 @@ class Quiz extends StatefulWidget {
 }
 
 class _QuizState extends State<Quiz> {
+  Widget activeScreen = const StartScreen();
+
+  void switchScreen() {
+    setState(() {
+      activeScreen = const QuestionsScreen();
+    });
+  }
+
   @override
   Widget build(context) {
-    return const MaterialApp(
+    return MaterialApp(
       home: Scaffold(
-        backgroundColor: Color.fromARGB(255, 151, 253, 221),
-        body: StartScreen(),
+        backgroundColor: const Color.fromARGB(255, 151, 253, 221),
+        body: activeScreen,
       ),
     );
   }
