4edd58d4597cf6aa322660cca3272ae865ab7443
John Knotts <jhnnyk@gmail.com>
2024-09-11 14:36:34 -0600

    accessing the actual answers

--- lib/questions_screen.dart
+++ lib/questions_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:sk8_quiz_app/answer_button.dart';
+import 'package:sk8_quiz_app/data/questions.dart';
 
 class QuestionsScreen extends StatefulWidget {
   const QuestionsScreen({super.key});
@@ -13,6 +14,8 @@ class QuestionsScreen extends StatefulWidget {
 class _QuestionsScreenState extends State<QuestionsScreen> {
   @override
   Widget build(context) {
+    final currentQuestion = questions[0];
+
     return SizedBox(
       // as an alternative to the Center widget
       // you can use SizedBox and set the width
@@ -21,24 +24,18 @@ class _QuestionsScreenState extends State<QuestionsScreen> {
       child: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
-          const Text(
-            'The question...',
-            style: TextStyle(fontSize: 20),
+          Text(
+            currentQuestion.text,
+            style: const TextStyle(fontSize: 20),
           ),
-          const SizedBox(
-            height: 30,
-          ),
-          AnswerButton(
-            answerText: 'answerText',
-            onTap: () {},
-          ),
-          AnswerButton(
-            answerText: 'answerText 2',
-            onTap: () {},
-          ),
-          AnswerButton(
-            answerText: 'answerText 3',
-            onTap: () {},
+          const SizedBox(height: 30),
+          ...currentQuestion.answers.map(
+            (answer) {
+              return AnswerButton(
+                answerText: answer,
+                onTap: () {},
+              );
+            },
           ),
         ],
       ),
